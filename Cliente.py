48fb0a8da31fa9fbf96271c790185f2c2d7bba87
0scar76 <oscarconejerosb@gmail.com>
2026-09-22 13:34:35 -0300

    feat: finished client and server

--- Cliente.py
+++ Cliente.py
@@ -1,16 +1,25 @@
 import socket
-import socketTCP
+#import sockeTCP
 import sys
 
-MAX_PACKET_SIZE = 16 
+MAX_PACKET_SIZE = 16
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        ip = sys.argv[1]
-        port = sys.argv[2] 
-    else:
-        print("Uso: python3 cliente.py <ip-servidor> <puerto>")
-        sys.exit(0)
+    SERVER_IP = "localhost"
+    SERVER_PORT = 8000
+    SERVER_ADRESS = (SERVER_IP, SERVER_PORT)
+    
+    client_socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    mensaje = input("Introduzca el mensaje a enviar")
+    mensaje = input("Introduzca el mensaje a enviar\n")
     mensaje = mensaje.encode("utf-8")
+
+    start = 0
+    chunk = mensaje[0:MAX_PACKET_SIZE]
+
+    while (len(chunk) != 0):
+           client_socket_UDP.sendto(chunk, SERVER_ADRESS)
+           start += MAX_PACKET_SIZE
+           chunk = mensaje[start:(start + MAX_PACKET_SIZE)]
+           print(start)
+           print(chunk)
