4eb907bac9baceb6a824650fe6efffe8396c1be2
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-09-22 12:57:43 -0300

    feat(cliente): done(?)

--- cliente.py
+++ cliente.py
@@ -2,9 +2,15 @@ import socket
 import sockeTCP
 impost sys
 
-MAX_PACKET_SIZE = 16 
+MAX_PACKET_SIZE = 16
 
 if __name__ == "__main__":
+    SERVER_IP = "arenarium"
+    SERVER_PORT = "8000"
+    SERVER_ADRESS = (SERVER_IP, SERVER_PORT)
+    
+    client_socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     if len(sys.argv) == 4:
         ip = sys.argv[1]
         port = sys.argv[2] 
@@ -15,4 +21,8 @@ if __name__ == "__main__":
     mensaje = input("Introduzca el mensaje a enviar")
     mensaje = mensaje.encode("utf-8")
 
+    iteration = 0
+    chunk = mensaje[iteration:(iteration + MAX_PACKET_SIZE)]
 
+    while (len(chunk) != 0):
+           client_socket_UDP.sendto(chunk, SERVER_ADRESS)
