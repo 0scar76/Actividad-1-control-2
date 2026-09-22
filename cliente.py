22d047ea3a8632742437bcaf515df00eafea90d6
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-09-22 12:39:31 -0300

    feat(cliente): expanded client

--- cliente.py
+++ cliente.py
@@ -2,13 +2,17 @@ import socket
 import sockeTCP
 impost sys
 
-if __name__ == "__main__":
+MAX_PACKET_SIZE = 16 
 
+if __name__ == "__main__":
     if len(sys.argv) == 4:
         ip = sys.argv[1]
-        port = sys.argv[2]
-        message = sys.argv[3]
-    
+        port = sys.argv[2] 
     else:
-        print("Uso: python3 cliente.py <ip-servidor> <puerto> <mensaje>")
+        print("Uso: python3 cliente.py <ip-servidor> <puerto>")
         sys.exit(0)
+
+    mensaje = input("Introduzca el mensaje a enviar")
+    mensaje = mensaje.encode("utf-8")
+
+
